6ccff7750a471833de41875abaf1bf147197d388
Mrastta <giftngure1@gmail.com>
2026-03-31 04:13:21 -0400

    Delete app directory

--- app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: 40 }}>
-      <h1>Welcome to your Trading Dashboard</h1>
-      <p>Your Next.js project is now running!</p>
-    </main>
-  );
-}
